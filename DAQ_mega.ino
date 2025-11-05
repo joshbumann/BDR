9b4e7809d64e2d56a9c8e25c9bc3085797add9c0
ThomasJur17 <149125382+ThomasJur17@users.noreply.github.com>
2025-11-05 16:59:42 -0700

    Working

--- DAQ_mega.ino
+++ DAQ_mega.ino
@@ -1,6 +1,12 @@
 #include <SoftwareSerial.h>
 #include <HX711.h>
 
+// Declare load cell objects
+HX711 loadcell1;
+HX711 loadcell2;
+HX711 loadcell3;
+
+
 // RS-485 pins on UNO
 // RS-485 communication
 const uint8_t RS485_RO_PIN = 13;    // RX from MAX485 RO (fallback)
@@ -24,6 +30,7 @@ inline void set485Talk() {            // RE=HIGH, DE=HIGH
 // Define analog pins for pressure sensors
 
 const int PT_pin7 = A6;
+
 const int PT_pin6 = A5;
 const int PT_pin5 = A4;
 const int PT_pin4 = A3;
@@ -31,18 +38,18 @@ const int PT_pin3 = A2;
 const int PT_pin2 = A1;
 const int PT_pin1 = A0;
 
-const int LC1_A = A35; // Needs two analog pins
-const int LC1_B = A37;
-const int LC2_A = A31; 
-const int LC2_B = A33;
-const int LC3_A = A27; 
-const int LC3_B = A29;
+const int LC1_A = 35; // Needs two analog pins
+const int LC1_B = 37;
+const int LC2_A = 31;
+const int LC2_B = 33;
+const int LC3_A = 27;
+const int LC3_B = 29;
 
 
 // Calibration factors (you’ll set these after calibration)
-float calFactor1 = 2900;  // Example values
+float calFactor1 = 2900;
 float calFactor2 = 6050;
-float calFactor3 = 7300.0;
+float calFactor3 = 7300;
 
 // initialize voltage values for speed
 int PT_read7 = 0;
@@ -57,10 +64,10 @@ String dataString = "null";
 void setup() {
 
   Serial.begin(115200);
-  
+
   pinMode(RS485_RE_PIN, OUTPUT);
   pinMode(RS485_DE_PIN, OUTPUT);
-  
+
   // Initialize RS485 communication (SoftwareSerial)
   set485Listen();              // idle in listen
   RS485Serial.begin(9600);
@@ -72,7 +79,7 @@ void setup() {
   pinMode(PT_pin3, INPUT);
   pinMode(PT_pin2, INPUT);
   pinMode(PT_pin1, INPUT);
-  
+
   pinMode(LC1_A, INPUT);
   pinMode(LC1_B, INPUT);
   pinMode(LC2_A, INPUT);
@@ -81,11 +88,11 @@ void setup() {
   pinMode(LC3_B, INPUT);
 
   loadcell1.begin(LC1_A, LC1_B);
-  loadcell1.begin(LC2_A, LC1_B);
-  loadcell1.begin(LC1_A, LC1_B);
+  loadcell2.begin(LC2_A, LC2_B);
+  loadcell3.begin(LC3_A, LC3_B);
 
   loadcell1.set_scale(calFactor1); // Adjust this value after calibration
-  loadcell1.tare();      // Reset the scale to zero
+  loadcell1.tare();                // Reset the scale to zero
   loadcell2.set_scale(calFactor2);
   loadcell2.tare();
   loadcell3.set_scale(calFactor3);
@@ -97,59 +104,58 @@ void setup() {
 
 
 void loop() {
-  
-    String dataString = "<";
-    
-    PT_read7 = analogRead(PT_pin7);
-    PT_read6 = analogRead(PT_pin6);
-    PT_read5 = analogRead(PT_pin5);
-    PT_read4 = analogRead(PT_pin4);
-    PT_read3 = analogRead(PT_pin3);
-    PT_read2 = analogRead(PT_pin2);
-    PT_read1 = analogRead(PT_pin1);
 
-    float Force1 = loadcell1.get_units(3);
-    float Force2 = loadcell2.get_units(3);
-    float Force3 = loadcell3.get_units(3);
+  String dataString = "<";
 
-    
-    dataString += String(PT_read1, 4);
-    dataString += ","; // csv
-    dataString += String(PT_read2, 4);
-    dataString += ",";
-    dataString += String(PT_read3, 4);
-    dataString += ",";
-    dataString += String(PT_read4, 4);
-    dataString += ",";
-    dataString += String(PT_read5, 4);
-    dataString += ",";
-    dataString += String(PT_read6, 4);
-    dataString += ",";
-    dataString += String(PT_read7, 4);
-    dataString += ",";
-    dataString += String(Force1, 4);
-    dataString += ",";
-    dataString += String(Force2, 4);
-    dataString += ",";
-    dataString += String(Force3, 4);
-    dataString += ">"; // End delimiter
-    dataString.trim();
-    
-    for(int i = 0; i < dataString.length(); ++i){
+  PT_read7 = analogRead(PT_pin7);
+  PT_read6 = analogRead(PT_pin6);
+  PT_read5 = analogRead(PT_pin5);
+  PT_read4 = analogRead(PT_pin4);
+  PT_read3 = analogRead(PT_pin3);
+  PT_read2 = analogRead(PT_pin2);
+  PT_read1 = analogRead(PT_pin1);
 
-          char cmd = dataString[i];
+  float Force1 = loadcell1.get_units(3);
+  float Force2 = loadcell2.get_units(3);
+  float Force3 = loadcell3.get_units(3);
 
-          if(cmd){
-            set485Talk();
-            RS485Serial.write(cmd);
-            //Serial.write(cmd);
-            RS485Serial.write('\n');
-            RS485Serial.flush();   // ensure bytes left TX buffer
-            delay(2);              // small guard at 9600 bps
-            set485Listen();
-          }
+
+  dataString += String(PT_read1, 4);
+  dataString += ","; // csv
+  dataString += String(PT_read2, 4);
+  dataString += ",";
+  dataString += String(PT_read3, 4);
+  dataString += ",";
+  dataString += String(PT_read4, 4);
+  dataString += ",";
+  dataString += String(PT_read5, 4);
+  dataString += ",";
+  dataString += String(PT_read6, 4);
+  dataString += ",";
+  dataString += String(PT_read7, 4);
+  dataString += ",";
+  dataString += String(Force1, 4);
+  dataString += ",";
+  dataString += String(Force2, 4);
+  dataString += ",";
+  dataString += String(Force3, 4);
+  dataString += ">"; // End delimiter
+  dataString.trim();
+
+  for (int i = 0; i < dataString.length(); ++i) {
+
+    char cmd = dataString[i];
+
+    if (cmd) {
+      set485Talk();
+      RS485Serial.write(cmd);
+      RS485Serial.write('\n');
+      RS485Serial.flush();   // ensure bytes left TX buffer
+      delay(2);              // small guard at 9600 bps
+      set485Listen();
     }
-    delay(150);
-  
+  }
+  delay(150);
+
 
 }
