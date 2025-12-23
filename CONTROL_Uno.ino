142ff6ff6a081bfbffc972eefd5c9792323e132b
ThomasJur17 <149125382+ThomasJur17@users.noreply.github.com>
2025-12-23 14:58:36 -0500

    Control_Uno - HF patch 1
    1st iteration of hotfire control code for the uno. 
    
    This file should provide basic functionality and be bug free. There could be some bugs and logic issues when we perform an integrated test. The code could also be cleaner.
    
    The uno will handle logic and transmit commands to the mega. The mega will receive these commands and double check the logic, before executing the commands on the stand.

--- CONTROL_Uno.ino
+++ CONTROL_Uno.ino
@@ -8,7 +8,9 @@ const uint8_t RS485_DI_PIN = 7;  // TX to MAX485 DI
 
 SoftwareSerial RS485Serial(RS485_RO_PIN, RS485_DI_PIN); // rx, tx
 
+bool hotfireMode = 0;
 bool statevec[8] = {0,0,0,0,1,1,0,0};
+int currentHFState = 0; // Track which sequences have been triggered (1-5)
 
 inline void set485Listen() {   // Receive mode: RE=LOW, DE=LOW
   digitalWrite(RS485_RE_PIN, LOW);
@@ -19,10 +21,21 @@ inline void set485Talk() {     // Transmit mode: RE=HIGH, DE=HIGH
   digitalWrite(RS485_DE_PIN, HIGH);
 }
 
-String stateStr = "";
+void transmitCommand(char cmd) {
+  set485Talk();
+  RS485Serial.write(cmd);
+  RS485Serial.write('\n');
+  RS485Serial.flush();   // ensure bytes left TX buffer
+  delay(2);              // small guard at 9600 bps
+  set485Listen();
+}
+
+void invalidInput() {
+  Serial.println("Invalid input. Please try again.");
+}
 
 void stateCheck(){
-  stateStr = "";
+  String stateStr = "";
   
   for(int i = 0; i<8; i++){
       stateStr = "";
@@ -90,53 +103,69 @@ void setup() {
   Serial.println(F("UNO RS485 Transmitter ready."));
 }
 
-
-
-void loop() {
-
-  if (Serial.available()) {
-
-    
-    Serial.println("Available valves: ");
-    
-    Serial.println("A) Fuel N2 Valve"); // pressurization valves
-    Serial.println("B) LOX N2 Valve"); // these might need to chars for the options
-    Serial.println("C) Main Fuel Valve");
-    Serial.println("D) Main Oxidizer Valve");
-    Serial.println("E) Fuel Vent");
-    Serial.println("F) LOX Vent");
-    Serial.println("G) Fuel Purge"); // purge valves
-    Serial.println("H) LOX Purge");
-    Serial.println("I) Toggle Main Valves");
-    Serial.println("J) Toggle Motorized(N2) Valves");
-    Serial.println("K) Toggle vent valves");
-    Serial.println("L) Toggle purge valves");
-    Serial.println("M) Close all valves");
-    Serial.println("N) Check Valve States");
-    
-    
-    Serial.println("Enter the option of the valve you want to toggle: ");
-    String command = Serial.readStringUntil('\n'); // Read until a newline character
+char getInput(){
+  string command = Serial.readStringUntil('\n'); // Read until a newline character
     command.trim(); // Remove any whitespace
     
     char cmd = 0;
    
         for (uint16_t i = 0; i < command.length(); ++i) {
           char c = command[i];
-          if (c >= 'A' && c <= 'N') { cmd = c; break; }
+          if ((c >= 'A' && c <= 'N') || (c >= '0' && c <= '5') || c == 'Y' || c == 'Q') { cmd = c; break; }
         }
+        return cmd;
+}
+
+
+void loop() {
+
+  if (Serial.available()) {
+
+    if(hotfireMode == 0){
+      Serial.println("Available valves: ");
+
+      Serial.println("A) Fuel N2 Valve"); // pressurization valves
+      Serial.println("B) LOX N2 Valve"); // these might need to chars for the options
+      Serial.println("C) Main Fuel Valve");
+      Serial.println("D) Main Oxidizer Valve");
+      Serial.println("E) Fuel Vent");
+      Serial.println("F) LOX Vent");
+      Serial.println("G) Fuel Purge"); // purge valves
+      Serial.println("H) LOX Purge");
+      Serial.println("I) Toggle Main Valves");
+      Serial.println("J) Toggle Motorized(N2) Valves");
+      Serial.println("K) Toggle vent valves");
+      Serial.println("L) Toggle purge valves");
+      Serial.println("M) Close all valves");
+      Serial.println("Q) Check Valve States");
+
+
+      Serial.println("Enter the option of the valve you want to toggle: ");
+    }
+    else if(hotfireMode == 1){
+      Serial.println("HOTFIRE MODE");
+      Serial.println("0) Return to coldflow");
+      Serial.println("1) Load Tanks");
+      Serial.println("2) Pressurize Tanks");
+      Serial.println("3) Begin Ignition Sequence (Requires confirmation)");
+      Serial.println("4) End/Abort Fire");
+      Serial.println("5) End Test - Return to Coldflow Mode");
+      Serial.println("Y) Confirm Command");
+      Serial.println("N) Cancel Command");
+      Serial.println("Q) Check Valve States");
+
+
+      Serial.println("Enter command: ");
+    }
+    cmd = getInput();
         
     
         if (cmd) {
-          if(cmd >= 'A' && cmd <= 'M'){
-            set485Talk();
-            RS485Serial.write(cmd);
-            RS485Serial.write('\n');
-            RS485Serial.flush();   // ensure bytes left TX buffer
-            delay(2);              // small guard at 9600 bps
-            set485Listen();
+          // Transmitted input
+          if(cmd >= 'A' && cmd <= 'M' && hotfireMode == 0){
+            transmitCommand(cmd);
 
-            
+
             switch(cmd){   // Keep track of states
               case 'A':
                 statevec[0] = !statevec[0];
@@ -182,16 +211,152 @@ void loop() {
                 for(int i = 0; i < 8; i++){
                   statevec[i] = 0;
                 }
-              break;
+                statevec[4] = 1;
+                statevec[5] = 1;
+                break;
               }
             
           }
-          else if(cmd == 'N'){
+          else if(cmd == 'Q'){
             stateCheck();
           }
+          else if((cmd >= '0' && cmd <= '5') || cmd == 'Y' || cmd == 'N'){
+            if(hotfireMode == 0 && cmd == '1'){
+              // enter hotfire mode
+              // currentHFState = 1
+              // transmit 1 to the mega
+              
+              hotfireMode = 1;
+              currentHFState = 1;
+              transmitCommand('1');
+
+              Serial.println("Entered hotfire mode.");
+              Serial.println("Available inputs:");
+              Serial.println("0) Return to coldflow state");
+              Serial.println("2) HFS2: Pressurize tanks");
+            }
+            else if(hotfireMode == 1 && currentHFState == 1){ // Handle HFS 1
+              if(cmd == '0'){
+                // go from state 1 to state 0
+
+                // exit hotfire mode
+                // currentHFState = 0
+                // transmit 0 to the mega
+                
+                hotfireMode = 0;
+                currentHFState = 0;
+                transmitCommand('0');
+              }
+              else if(cmd == '2'){
+                // go from state 1 to state 2
+
+                // currentHFState = 2
+                // transmit 2 to the mega
+                
+                currentHFState = 2;
+                transmitCommand('2');
+
+                Serial.println("Pressurizing tanks.");
+                Serial.println("Verify correct PT data before proceeding.");
+                Serial.println("Available inputs:");
+                Serial.println("0) Return to coldflow state");
+                Serial.println("1) HFS1: Depressurize tanks");
+                Serial.println("3) HFS3: BEGIN IGNITION SEQUENCE (Requires confirmation)");
+              }
+              else{
+                invalidInput();
+              }
+            }
+            else if(hotfireMode == 1 && currentHFState == 2){ // Handle HFS 2
+              if(cmd == '0'){
+                // go from state 2 to state 0
+
+                // exit hotfire mode
+                // currentHFState = 0
+                // transmit 0 to the mega
+                
+                hotfireMode = 0;
+                currentHFState = 0;
+                transmitCommand('0'); // MEGA NEEDS TO HANDLE 
+              }
+              else if(cmd == '1'){
+                // go from state 2 to state 1
+
+                // currentHFState = 1
+                // transmit 1 to the mega
+                
+                currentHFState = 1;
+                transmitCommand('1');
+
+                Serial.println("Tanks depressurizing");
+                Serial.println("Verify with PT data before proceeding");
+                Serial.println("Available inputs:");
+                Serial.println("0) Return to coldflow state");
+                Serial.println("2) HFS2: Pressurize tanks");
+              }
+              else if(cmd == '3'){
+                // go from state 2 to state 3
+                currentHFState = 3;
+                transmitCommand('3');
+
+                Serial.println("Confirm ignition sequence (Y/N)");
+                cmdHF = getInput();
+                if(cmdHF == 'N'){
+                  transmitCommand('N');
+                  currentHFState = 2;
+                  transmitCommand('2');
+                }
+                else if(cmdHF == 'Y'){
+                  transmitCommand('Y');
+                  Serial.println("Available inputs:");
+                  Serial.println("4) ABORT / end test");
+                }
+              }
+              else{
+                invalidInput();
+              }
+            }
+            else if(hotfireMode == 1 && currentHFState == 3){ // Handle HFS 3 (Cant go back from here)
+              if(cmd == '4'){
+                // currentHFState = 4
+                // transmit 4 to the mega
+                
+                currentHFState = 4;
+                transmitCommand('4');
+
+                Serial.println("Test over.");
+                Serial.println("Available inputs:");
+                Serial.println("5) Return to coldflow state");
+              }
+              else{
+                invalidInput();
+              }
+
+            }
+            else if(hotfireMode == 1 && currentHFState == 4){ // Handle HFS 4
+              if(cmd == '5'){
+                // go from state 4 to state 5 (End test)
+
+                // hotfireMode off
+                // currentHFState = 0
+                // transmit 5 to the mega
+                
+                hotfireMode = 0;
+                currentHFState = 0;
+                transmitCommand('5');
+              }
+              else{
+                invalidInput();
+              }
+              
+            }
+            else{
+              invalidInput();
+            }
+            
         }
         else {
-          Serial.println("Ignored. Send a single digit 1..8.");
+          invalidInput();
         }
   }
 }
